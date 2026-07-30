6e1a57495725b89c71686667f16c9138e8720fc2
CJam76 <christianflynn05@gmail.com>
2026-07-30 11:46:12 -0500

    Challenge 1 file for end of chapter.

---
+++ Chapter 3/Challenge_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  for (int i = 0; i < 5; i++) {
+    puts("Hello World!");
+  }
+  int i = 5;
+  while(i > 5) {
+    puts("Hello World!");
+    i = i - 1;
+  }
+}
