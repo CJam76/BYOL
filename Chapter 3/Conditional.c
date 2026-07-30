05c1cad75258190e8ed15059f4755a5922405c8b
CJam76 <christianflynn05@gmail.com>
2026-07-30 08:34:57 -0500

    Files for struct and conditional examples from chapter 3.

---
+++ Chapter 3/Conditional.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int x;
+  if (x > 10 && x < 100) {
+    puts("x is greater than 10 and less than 100");
+  } else {
+    puts("x is less than 11 or greater than 99");
+  }
+}
---
+++ Chapter 3/Structures.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  typedef struct {
+    float x;
+    float y;
+  } point;
+
+  point p;
+
+  p.x = 0.1;
+  p.y = 10.0;
+
+  float length = sqrt(p.x * p.x + p.y * p.y);
+}
